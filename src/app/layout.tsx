09d29a12e895739393de5e6488367a8d6ff17210
malinjr07 <malinjr07@gmail.com>
2024-03-08 19:57:14 +0600

    Search Bar UI Completed

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,16 @@
-import type { Metadata } from 'next';
-import { Poppins } from 'next/font/google';
-import './globals.css';
+import type { Metadata } from "next";
+import { Poppins } from "next/font/google";
+import "./globals.css";
+import "./all.min.css";
 
 const poppins = Poppins({
-  subsets: ['latin'],
-  weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
-  style: 'normal',
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+  style: "normal",
 });
 
 export const metadata: Metadata = {
-  title: 'E Shop Now | One E-Commerce For All',
+  title: "E Shop Now | One E-Commerce For All",
   description:
     "The One and Only E-Commerce Project that you'll ever dream about, the E Shop Now. With Maximum PageSpeed, GTmatrix Record, Most SEO, Server Optimized Application which will not only increase sells, but also reduce the Server Cost like no other application can! ",
 };
@@ -20,9 +21,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang='en'>
+    <html lang="en">
       <body className={poppins.className}>{children}</body>
     </html>
   );
 }
-
--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,11 +1,41 @@
-import Image from "next/image";
 import React, { FC } from "react";
 import Logo from "./icons/Logo";
+import User from "./icons/User";
+import Basket from "./icons/Basket";
 
 const Header: FC = () => {
   return (
-    <header className="container flex justify-between py-10 ">
+    <header className="container flex items-center justify-between py-5 ">
       <Logo />
+      <div className="flex h-fit flex-row items-center justify-start rounded-xl border border-iron bg-alabaster px-4 py-2.5 ">
+        <button
+          type="button"
+          className="flex flex-row items-center justify-start gap-2 text-base font-bold "
+        >
+          <p className="capitalize text-woodsmoke ">all categories</p>
+          <i className="fa-solid fa-angle-down m-1.5 text-base text-olive-drab" />
+        </button>
+        <span className="mx-6 h-5 w-0.5 bg-iron " />
+        <input
+          type="text"
+          name=""
+          id=""
+          className="bg-transparent w-52 overflow-hidden text-ellipsis whitespace-nowrap placeholder:capitalize placeholder:text-silver-chalice "
+          placeholder="Search products, categories, brands, sku, filter options"
+        />
+        <i className="fa-solid fa-magnifying-glass ml-16 rotate-90 cursor-pointer text-base text-woodsmoke "></i>
+      </div>
+      <div className="flex flex-row items-center justify-start gap-10 ">
+        <button className="">
+          <User />
+        </button>
+        <button type="button" className="relative">
+          <Basket />
+          <span className="text-white absolute -bottom-1 left-0 h-4 w-4 rounded-full bg-burnt-sienna text-xs font-semibold shadow-md ">
+            5
+          </span>
+        </button>
+      </div>
     </header>
   );
 };
