79c5c33d0d2b264e1f8d4ab9d81a4316cc19807c
malinjr07 <malinjr07@gmail.com>
2024-03-17 10:12:57 +0600

    card 03

---
+++ src/components/cards/Card03.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Card03 = () => {
+  return (
+    <div className="borderGradient h-fit w-fit cursor-pointer rounded-[20px] transition-all duration-300 ">
+      <div className="hover:rounded-20px h-[250px] w-[190px] rounded-[15px] bg-[#1a1a1a] p-1 transition-all duration-200 hover:scale-95 "></div>
+    </div>
+  );
+};
+
+export default Card03;
